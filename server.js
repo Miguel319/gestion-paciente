d398cbbcbfaefbc72a57e1e1ef1bcbfc7790c2ee
Miguel319 <miguelp90.319@gmail.com>
2020-03-27 18:30:21 -0400

    Heroku Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + "/dist/gestion-pacientes"));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/gestion-pacientes/index.html"));
+});
+
